dc571ab869ad209018ae0073f16ee3f43ac48fc4
Jihan <jihanmckenzie@gmail.com>
2019-05-11 14:34:55 -0400

    "completed the sum, found total using loop"

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,24 @@
 #import modules 
-import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = ('budget_data.csv')
+total_months = 0
 #open the file
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    #check that it reads
     print(csvreader)
+    #run loop to find sum
+    for row in csvreader:
+        total_months = total_months + 1
+print(total_months)
+
+
+
+    
+
+    
+
+
 
 
 
@@ -13,4 +26,3 @@ with open(csvpath, newline='') as csvfile:
 
 
     
-
